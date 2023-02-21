7a1297857d004f9619489c7ea940d6e328e19c8e
HungryStudent <nikita.efan.2016@mail.ru>
2023-02-21 14:47:42 +0300

    fix pay

--- pay_server.py
+++ pay_server.py
@@ -12,18 +12,19 @@ app = FastAPI()
 @app.post('/api/pay')
 async def check_pay(req: Request):
     data = await req.json()
-    order_id = int(data["OrderId"])
-    db.change_order_status(order_id)
-    order = db.get_order(order_id)
-    if order is None:
-        return "OK"
-    price = db.get_price(order["price_id"])
-    await bot.send_message(order["user_id"],
-                           "Ваша карта оплачена и придет в этот чат в течении 15 минут, если что, ответим на все вопросы прям в этом чате, хорошего дня)")
-    await bot.send_message(manager_id, f"""Новая заявка на карту:
-Пользователь: @{order["username"]}
-Карта номиналом {price['nominal']}
-На сумму {price['amount']}""", reply_markup=admin_kb.get_give_code(order["user_id"]))
+    if data["Status"] == "CONFIRMED":
+        order_id = int(data["OrderId"])
+        db.change_order_status(order_id)
+        order = db.get_order(order_id)
+        if order is None:
+            return "OK"
+        price = db.get_price(order["price_id"])
+        await bot.send_message(order["user_id"],
+                               "Ваша карта оплачена и придет в этот чат в течении 15 минут, если что, ответим на все вопросы прям в этом чате, хорошего дня)")
+        await bot.send_message(manager_id, f"""Новая заявка на карту:
+    Пользователь: @{order["username"]}
+    Карта номиналом {price['nominal']}
+    На сумму {price['amount']}""", reply_markup=admin_kb.get_give_code(order["user_id"]))
     return "OK"
 
 
